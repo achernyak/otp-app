f5057f9ebf3c0651df69fbbab6f24f9663d3923b
achernyak <artemchernyak@gmail.com>
2016-03-01 20:35:07 -0600

    versioning

--- sequence/lib/sequence.ex
+++ sequence/lib/sequence.ex
@@ -2,6 +2,6 @@ defmodule Sequence do
   use Application
 
   def start(_type, _args) do
-    {:ok, _pid} = Sequence.Supervisor.start_link(Application.get_env(:sequence, :intial_number))
+    {:ok, _pid} = Sequence.Supervisor.start_link(Application.get_env(:sequence, :initial_number))
   end
 end
--- sequence/mix.exs
+++ sequence/mix.exs
@@ -3,7 +3,7 @@ defmodule Sequence.Mixfile do
 
   def project do
     [app: :sequence,
-     version: "0.0.1",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -32,6 +32,8 @@ defmodule Sequence.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:exrm, "~> 1.0"}
+    ]
   end
 end
